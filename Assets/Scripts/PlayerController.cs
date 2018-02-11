fdda9788c38bdf1a0c82fd4ddb89672a42059e01
Mario40000 <mario40000@hotmail.com>
2018-02-11 20:13:37 +0100

    Nivel Completo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0.0f;
+    public float move = 0.0f;
+
+    void FixedUpdate ()
+    {
+        move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
+        if(Input.GetButton("Jump"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+        }
+    }
+	
+}
